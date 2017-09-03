d87bce4c9c6113c2160b26b2164ba4346e3fa257
Michael Jackson <mj94@mj94.net>
2017-09-03 17:30:44 -0500

    Randomize DiceImageView1 when Roll button is pushed

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
     
     var randomDiceIndex1 = 0
     var randomDiceIndex2 = 0
+    
+    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
 
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
@@ -30,6 +32,8 @@ class ViewController: UIViewController {
     @IBAction func rollButtonPushed(_ sender: UIButton) {
         randomDiceIndex1 = Int(arc4random_uniform(6))
         randomDiceIndex2 = Int(arc4random_uniform(6))
+        
+        diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
         
     }
 }
