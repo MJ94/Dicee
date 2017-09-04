daccfe0c4a7154a107f47f3a72c2129ca5e7a997
Michael Jackson <mj94@mj94.net>
2017-09-03 20:29:51 -0500

    Create function for randomization code

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
     var randomDiceIndex2 = 0
     
     let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
-
+    
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
@@ -22,20 +22,24 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
     @IBAction func rollButtonPushed(_ sender: UIButton) {
+        updateDiceImages()
+    }
+    
+    func updateDiceImages() {
         randomDiceIndex1 = Int(arc4random_uniform(6))
         randomDiceIndex2 = Int(arc4random_uniform(6))
         
         diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
         diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
-        
     }
+    
 }
 
