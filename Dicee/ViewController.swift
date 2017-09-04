0bcf51e2739c9dadca233e429877936e692995fb
Michael Jackson <mj94@mj94.net>
2017-09-03 20:35:32 -0500

    Add updateDiceImages function to viewDidLoad()

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -20,7 +20,8 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        updateDiceImages()
     }
     
     override func didReceiveMemoryWarning() {
